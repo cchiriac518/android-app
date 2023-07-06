cca0c2fb30a36d6c4ef4561e8b319f348a67a643
cchiriacc <cristian.chiriac@tremend.com>
2023-07-06 17:11:18 +0300

    added java code

---
+++ src/com/example/MyLibrary.java
@@ -0,0 +1,10 @@
+// MyLibrary.java
+public class MyLibrary {
+    public static void sayHello() {
+        System.out.println("Hello from MyLibrary!");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
